54ad5617cd241b466062d6232719a04af7682c6a
Scode0 <gandumdiagne.serena@gmail.com>
2025-07-17 04:17:49 +0200

    Use numpy for forward propagation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -9,16 +9,15 @@ class NeuralNetwork:
         self.output_hidden_weights = np.random.rand(2)
         self.output_bias = np.random.rand()
     
-    def forward_propagation(self, input1, input2):
-        z = calculations.linear_activation(self.hidden_weights, self.hidden_bias, input1, input2)
-        a1 = calculations.sigmoid(z[0])
-        a2 = calculations.sigmoid(z[1])
+    def forward_propagation(self, inputs):
+        z = calculations.linear_activation(inputs, self.hidden_weights, self.hidden_bias)
+        a = calculations.sigmoid(z)
 
-        z = calculations.linear_activation_output(self.output_hidden_weights, self.output_bias, a1, a2)
+        z = calculations.linear_activation(a, self.output_hidden_weights, self.output_bias)
         a = calculations.sigmoid(z)
 
         return a
 
     def train(self):
-        for inpt in XOR.inputs:
-            output = self.forward_propagation(inpt[0], inpt[1])
+        for inputs in XOR.inputs:
+            output = self.forward_propagation(inputs)
--- calculations.py
+++ calculations.py
@@ -7,12 +7,5 @@ def derivative_sigmoid(z):
     s = sigmoid(z)
     return s * (1 - s)
 
-def linear_activation(w, b, input1, input2):
-    z1 = input1 * w[0, 0] + input2 * w[1, 0] + b[0]
-    z2 = input1 * w[0, 1] + input2 * w[1, 1] + b[1]
-    z = [z1, z2]
-
-    return z
-
-def linear_activation_output(w, b, input1, input2):
-    return input1 * w[0] + input2 * w[1] + b
+def linear_activation(inputs, weights, bias):
+    return np.dot(inputs, weights) + bias
